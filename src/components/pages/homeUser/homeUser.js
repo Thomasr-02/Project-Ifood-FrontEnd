b6263b57d318221fbb89a204b096061584693f08
Neithan <nathangomes@mat.ci.ufpb.br>
2020-04-01 22:36:33 -0300

    pesquisa de pratos ou restaurantes funcionando

--- src/components/pages/homeUser/homeUser.js
+++ src/components/pages/homeUser/homeUser.js
@@ -10,7 +10,8 @@ export default class Home extends Component {
     state = {
         restaurantes: [],
         search: '',
-        id_establishment: 0
+        id_establishment: 0,
+        pratos: []
     }
 
     Logout() {
@@ -19,39 +20,56 @@ export default class Home extends Component {
     }
 
     componentDidMount () {
-        api.get('/findProductOrRestaurante/'+ this.state.search)
-                .then(res => {
-                    const restaurantes = res.data;
-                    this.setState( { restaurantes } );
-                
-                }).catch((err) => {
-                    console.log(err)
-         });
+        api.get('/findProductOrRestaurante/')
+        .then(res => {
+            const restaurantes = res.data;
+            this.setState( { restaurantes } );
+        
+        }).catch((err) => {
+            console.log(err)
+        });
     }
     
     handle = async e => {
         await this.setState({[ e.target.name] : e.target.value})
     }
-
-    handleChange = async e => {
-        var search = e.target.value
-        await this.setState({ search: search })
-
-        api.get('/findProductOrRestaurante/' + this.state.search)
-            .then(res => {
-                const restaurantes = res.data;
-                console.log(restaurantes)
-                this.setState( { restaurantes } );
-            
-            }).catch((err) => {
-                console.log(err)
-        });
-    }
     
     sendIdCompras = (e) => {
 
         intermediador.idRest(e.target.value, "set")
-        intermediador.idUser()
+        // intermediador.idUser()
+    }
+
+    mostraPratos = () => {
+        var name_dish = this.state.search
+        console.log(name_dish)
+        api.get('/findProduct/' + name_dish)
+        .then(res => {
+            const dish = res.data;
+            console.log(dish)
+            this.setState({ restaurantes: [] })
+            this.setState({ pratos: dish })
+
+        }).catch(err => {
+            console.log(err)
+
+        })
+    } 
+
+    mostraRestaurantes =() => {
+        var name_estab = this.state.search
+
+        api.get('/findRestaurante/' +  name_estab)
+        .then(res => {
+            const restaurantes = res.data;
+            console.log(restaurantes)
+            this.setState({ pratos: [] })
+            this.setState({ restaurantes })
+
+        }).catch(err => {
+            console.log(err)
+
+        })
     }
 
   render () {
@@ -60,12 +78,21 @@ export default class Home extends Component {
           <h1 className="home-tittle">Home cliente</h1>
           <div className="naveHomeUser">
                 <div className="container-search">
-                    <input className='search' id="Search" onChange={this.handleChange} type="search" placeholder="Pesquisa restaurante/prato"></input>
+                    <input className='search' name='search' onChange={ this.handle } type="search" placeholder="Pesquisa restaurante/prato"></input>
                     <img src={require("../../../assets/search.png")} width="29" height="29" alt="search"></img>
                     <Link to="/" className="button-logout">
                         <button onClick={this.Logout}>Logout</button>
                     </Link>
                 </div>
+                <div>
+                    <div className="button-position">
+                        <button id="button-pesquisa-prato" onClick={ this.mostraPratos }>Pesquisar prato</button>
+                        <button onClick={ this.mostraRestaurantes }>Pesquisar restaurante</button>
+                    </div>
+                    
+                </div>
+                
+
           </div>
           
           <div className="grid-container">
@@ -83,14 +110,15 @@ export default class Home extends Component {
                     
                 </div>
             </div>
-            {/* cards */}
+
             <div className="cardsRestaurantes">
-                <h3>Restaurantes</h3>
+                <h3>Pesquisa</h3>
                 <br></br>
                 { 
                     this.state.restaurantes.reverse().map (restaurantes => (
-                    <div key={restaurantes.id_establishment} className = "container-cards-restaurantes">
+                    <div key={restaurantes.id_establishment} className = "container-cards-cardapio">
                         
+
                         <div className="cards">
                             <h5> <b>{restaurantes.name_estab}</b></h5>
                             <p>Cidade: {restaurantes.city}</p>
@@ -100,18 +128,31 @@ export default class Home extends Component {
                                 <button name="id_establishment" type="submit" onClick={ this.sendIdCompras } value={ restaurantes.id_establishment } className="btn btn-primary" >  Ver mais </button>
                             </Link>
                             
+                        </div>
+                    </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+                    ))
+                }
+                
+                { 
+                    this.state.pratos.map (pratos => (
+                    <div key={pratos.id_dish} className = "container-cards-cardapio" >
+                        
+                        <div className="cards">
+                            <h5> <b>{pratos.name_dish}</b></h5>
+                            <p>Valor: {pratos.value_dish}</p>
+                            
+                            <p></p>
+                            <Link to = "/homeUser/compras">
+                                <button name="id_dish" type="submit" onClick={ this.sendIdCompras } value={ pratos.id_establishment } className="btn btn-primary" >  Ver mais </button>
+                            </Link>
                             
                         </div>
                     </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
                     ))
                 }
-
+            
             </div>
-            
-            
           </div>
-          
-          {/* <Compras id_establishment={ this.state.id_establishment }/> */}
       </div>
     );
   }
